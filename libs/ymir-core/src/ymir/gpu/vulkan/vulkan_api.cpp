55535abbce7e87608c750684db6260ab8d09ad3e
Wunkolo <Wunkolo@gmail.com>
2026-08-19 13:00:08 -0700

    feat(gfx-vulkan): Statically initialize vulkan dispatch

--- libs/ymir-core/src/ymir/gpu/vulkan/vulkan_api.cpp
+++ libs/ymir-core/src/ymir/gpu/vulkan/vulkan_api.cpp
@@ -2,4 +2,13 @@
 
 VULKAN_HPP_DEFAULT_DISPATCH_LOADER_DYNAMIC_STORAGE;
 
+// Initialize vulkan's function-pointers as early as possible
+static const bool vulkan_loader = []() -> bool {
+    static vk::detail::DynamicLoader DynamicLoader;
+
+    VULKAN_HPP_DEFAULT_DISPATCHER.init(
+        DynamicLoader.getProcAddress<PFN_vkGetInstanceProcAddr>("vkGetInstanceProcAddr"));
+    return true;
+}();
+
 namespace ymir::gpu::vulkan {}
