07d6a7cab25f72325dd886b7ca48c221bfb04317
Wunkolo <Wunkolo@gmail.com>
2026-08-21 21:01:55 -0700

    feat(gfx-vulkan): Use unique quad shader binding locations
    Required for vulkan where the vert and frag shader need to all touch unique binding numbers for the same set

--- apps/ymir-sdl3/res/shaders/gctx/quad_vs.hlsl
+++ apps/ymir-sdl3/res/shaders/gctx/quad_vs.hlsl
@@ -1,6 +1,6 @@
 #include "quad_defs.hlsli"
 
-ConstantBuffer<DrawTextureConstants> g_consts : register(b0);
+[[vk::binding(0)]] ConstantBuffer<DrawTextureConstants> g_consts : register(b0);
 
 PSInput VSMain(float4 position : POSITION, float2 uv : TEXCOORD) {
     PSInput result;
