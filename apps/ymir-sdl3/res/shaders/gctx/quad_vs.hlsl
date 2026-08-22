a8ff5d724a984d9cfc8aa797d5e8f578eb7d31ca
Wunkolo <Wunkolo@gmail.com>
2026-08-22 08:00:39 -0700

    feat(gfx-vulkan): Update quad shader vulkan attributes
    The vulkan version of some of these DirectX types is to utilize a
    combined image sampler and ensure that the texture and sampler use the same spots. Also mapping the ConstantBuffer to
    vulkan push-constants

--- apps/ymir-sdl3/res/shaders/gctx/quad_vs.hlsl
+++ apps/ymir-sdl3/res/shaders/gctx/quad_vs.hlsl
@@ -1,6 +1,6 @@
 #include "quad_defs.hlsli"
 
-[[vk::binding(0)]] ConstantBuffer<DrawTextureConstants> g_consts : register(b0);
+[[vk::push_constant]] ConstantBuffer<DrawTextureConstants> g_consts : register(b0);
 
 PSInput VSMain(float4 position : POSITION, float2 uv : TEXCOORD) {
     PSInput result;
